5db3e628e5b7bb45185ff6705913859af00a52dd
sujon-mia <sujonmia.0612@gmail.com>
2022-04-20 23:22:59 +0600

    node primary

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('hello nodejs. i will be the node developer InShaAllah');
+});
+
+const users = [
+    { id: 1, name: 'Jon', email: 'jon@email.com', phone: '123-456-1234' },
+    { id: 2, name: 'Mon', email: 'mon@email.com', phone: '123-456-1234' },
+    { id: 3, name: 'Kon', email: 'kon@email.com', phone: '123-456-1234' },
+    { id: 4, name: 'Rok', email: 'rok@email.com', phone: '123-456-1234' },
+    { id: 5, name: 'Fok', email: 'fok@email.com', phone: '123-456-1234' },
+    { id: 6, name: 'Jog', email: 'jog@email.com', phone: '123-456-1234' },
+    { id: 7, name: 'Mog', email: 'mog@email.com', phone: '123-456-1234' },
+]
+
+app.get('/users', (req, res) => {
+    // filter by search query parameter
+    if (req.query.name) {
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter(user => user.name.toLowerCase().includes(search))
+        res.send(matched);
+    }
+    else {
+        res.send(users);
+    }
+});
+
+app.get('/user/:id', (req, res) => {
+    console.log(req.params.id);
+    const id = parseInt(req.params.id);
+    const user = users.find(user => user.id === id);
+    res.send(user);
+});
+
+app.post('/user', (req, res) => {
+    console.log('request', req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user);
+})
+
+app.get('/fruits', (req, res) => {
+    res.send(['mango', 'orange', 'apple'])
+});
+
+app.get('/fruits/mango/fozli', (req, res) => {
+    res.send('sour fozli flavour')
+})
+
+app.listen(port, () => {
+    console.log('listening on port', port)
+});
